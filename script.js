a410bb0931c0cf792d9a502809ef5e2b32b142c8
Shahzaibkhan0720 <162187257+Shahzaibkhan0720@users.noreply.github.com>
2025-04-22 01:20:25 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+<!-- JavaScript -->
+<script>
+    document.querySelectorAll('.project-card').forEach(card => {
+        card.addEventListener('click', () => {
+            alert('Project details will be shown here.');
+        });
+    });
+</script>
